06cf1adcc95d089fd049010a54ae58e74748476e
Yvo Brevoort <yvo@muze.nl>
2017-12-20 09:05:12 +0100

    initial commit

---
+++ github-login.js
@@ -0,0 +1,41 @@
+editor.storageConnectors.github.connect = function(callback) {
+	if (!document.getElementById("simply-login")) {
+		callback();
+	}
+
+	document.querySelector("#simply-login .simply-message").innerHTML = "Login with Github:";
+
+	editor.plugins.dialog.open(document.getElementById('simply-login'));
+	window.setTimeout(function() {
+		document.querySelector("#simply-login input").focus();
+	}, 10);
+	var handleLogin = function(evt) {
+		evt.preventDefault();
+		editor.plugins.dialog.close();
+		document.querySelector("#simply-login input[value=Login]").removeEventListener("click", handleLogin);
+		document.querySelector("#simply-login input[value=Cancel]").removeEventListener("click", handleLogin);
+
+		if (evt.target.getAttribute("value") == 'Login') {
+			editor.storage.github = new Github({
+				username: evt.target.form.username.value,
+				password: evt.target.form.password.value
+			});
+			editor.storage.repo = editor.storage.github.getRepo(editor.storage.repoUser, editor.storage.repoName);
+			callback();
+		}
+	};
+	document.querySelector("#simply-login input[value=Login]").addEventListener("click", handleLogin);
+	document.querySelector("#simply-login input[value=Cancel]").addEventListener("click", handleLogin);
+};
+editor.loadToolbar("https://canary.simplyedit.io/1/github/simply/plugin.simply-login.html");
+
+document.addEventListener("simply-toolbars-loaded", function() {
+	editor.storage.connect(function() {
+		editor.storage.repo.read(editor.storage.repoBranch, "data.json", function(err, data) {
+			if (data) {
+				editor.currentData = JSON.parse(data);
+				editor.data.apply(editor.currentData, document);
+			}
+		});
+	});
+});
